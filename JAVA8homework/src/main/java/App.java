f83bbcde5caf87937d98b2f2b751a9acfc8b1a10
Ryan.Wang <ranwang1995@outlook.com>
2020-03-06 03:52:49 +0800

    [WangRan] feat: finish main method under class App

--- JAVA8homework/src/main/java/App.java
+++ JAVA8homework/src/main/java/App.java
@@ -1,10 +1,25 @@
+import entity.MasterNumber;
+import entity.Person;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class App {
 
-  public static void main(String[] args) {
-    PersonService personService = new PersonService();
-    //TODO: print Person data that masterNumber is 1 and 2
-    // print Person data that masterNumber is 3
+    public static void main(String[] args) {
+        PersonService personService = new PersonService();
+        //TODO: print Person data that masterNumber is 1 and 2
+        // print Person data that masterNumber is 3
 
-  }
+        Stream<Person> person12 = personService.getPersonByMasterNumbers(Arrays.asList(
+                new MasterNumber("1"), new MasterNumber("2")));
+        Stream<Person> person3 = personService.getPersonByMasterNumbers(Arrays.asList(new MasterNumber("3")));
+        System.out.println("==Person with masterNumber 1&2==");
+        person12.forEach(System.out::println);
+        System.out.println("================================");
+        System.out.println("==Person with masterNumber 3====");
+        person3.forEach(System.out::println);
+        System.out.println("================================");
+    }
 
 }
