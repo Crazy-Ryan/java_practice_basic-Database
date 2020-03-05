64445d8c837d46640ab7bcba46b3c602d08a8e14
Ryan.Wang <ranwang1995@outlook.com>
2020-03-06 00:27:23 +0800

    [WangRan] feat: add getters and override equals and hashcode methods

--- JAVA8homework/src/main/java/entity/Email.java
+++ JAVA8homework/src/main/java/entity/Email.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.Objects;
+
 public class Email {
 
   private String masterNumber;
@@ -15,4 +17,25 @@ public class Email {
     this.masterNumber = masterNumber;
   }
 
+  public String getMasterNumber() {
+    return masterNumber;
+  }
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Email email = (Email) o;
+    return Objects.equals(masterNumber, email.masterNumber) &&
+            Objects.equals(emailAddress, email.emailAddress);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(masterNumber, emailAddress);
+  }
 }
