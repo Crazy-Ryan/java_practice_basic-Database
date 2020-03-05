ae50d119e3cc61a875ba18acfad137928d56bef1
Ryan.Wang <ranwang1995@outlook.com>
2020-03-06 00:26:48 +0800

    [WangRan] feat: override equals and hashcode methods

--- JAVA8homework/src/main/java/entity/Address.java
+++ JAVA8homework/src/main/java/entity/Address.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.Objects;
+
 public class Address {
 
   private String masterNumber;
@@ -35,6 +37,22 @@ public class Address {
 
   public String getStreet() {
     return street;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Address address = (Address) o;
+    return Objects.equals(masterNumber, address.masterNumber) &&
+            Objects.equals(country, address.country) &&
+            Objects.equals(city, address.city) &&
+            Objects.equals(street, address.street);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(masterNumber, country, city, street);
   }
 }
 
